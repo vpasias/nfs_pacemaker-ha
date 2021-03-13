294cad2e557c7bdb96417bcd7ace1153773f2982
vpasias <52623837+vpasias@users.noreply.github.com>
2021-03-13 11:19:20 +0200

    Update node_setup.sh

--- node_setup.sh
+++ node_setup.sh
@@ -51,6 +51,26 @@ cat << EOF | tee /etc/hosts
 172.16.2.104 server402
 EOF
 
+cat << EOF | tee /etc/ganesha/ganesha.conf
+EXPORT{
+    Export_Id = 1 ;       # Unique identifier for each EXPORT (share)
+    Path = "/sharedvol";  # Export path of our NFS share
+
+    FSAL {
+        name = GLUSTER;          # Backing type is Gluster
+        hostname = "localhost";  # Hostname of Gluster server
+        volume = "sharedvol";    # The name of our Gluster volume
+    }
+
+    Access_type = RW;          # Export access permissions
+    Squash = No_root_squash;   # Control NFS root squashing
+    Disable_ACL = FALSE;       # Enable NFSv4 ACLs
+    Pseudo = "/sharedvol";     # NFSv4 pseudo path for our NFS share
+    Protocols = "3","4" ;      # NFS protocols supported
+    Transports = "UDP","TCP" ; # Transport protocols supported
+    SecType = "sys";           # NFS Security flavors supported
+}EOF
+
 apt update -y
 
 reboot
